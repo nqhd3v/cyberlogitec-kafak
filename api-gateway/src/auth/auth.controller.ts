569d8b4d87d76f6fade16e1ee9f997dc486aa5a6
Nguyen Quoc Huy <nqh.d3v@gmail.com>
2023-02-12 09:59:46 +0700

    Commit to reproduce issue when using 'UseGaurds' in gateway app

--- api-gateway/src/auth/auth.controller.ts
+++ api-gateway/src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { UserService } from 'src/user/user.service';
 import { Http400Exception } from 'utils/Exception/http-400.exception';
 import { Http503Exception } from 'utils/Exception/http-503.exception';
@@ -32,7 +31,7 @@ export class AuthController {
     }
   }
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async validUser(@Req() data: iReqWithUser) {
     try {
